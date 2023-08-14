f5b4ba8b76d18dad13b0db49c61781902ba59517
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-08-14 20:29:48 +0530

    import java.util.Scanner; public class mono {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t!=0)         {             int a=sc.nextInt();             int b=sc.nextInt();             int c=sc.nextInt();             if(a>b+c || b>c+a || c>a+b)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }             t-=1;         }     } }

--- Monopoly.java
+++ Monopoly.java
@@ -4,13 +4,13 @@ public class mono
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int test=sc.nextInt();
-        while(test!=0)
-        {   int a,b,c;
-            a=sc.nextInt();
-            b=sc.nextInt();
-            c=sc.nextInt();
-            if ((a+b)<c || (b+c)<a || (c+a)<b)
+        int t=sc.nextInt();
+        while(t!=0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a>b+c || b>c+a || c>a+b)
             {
                 System.out.println("YES");
             }
@@ -18,8 +18,7 @@ public class mono
             {
                 System.out.println("NO");
             }
-        test-=1;
+            t-=1;
         }
-
     }
 }
