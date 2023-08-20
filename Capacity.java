b22faebce14152e7aabd1404afcd1de032f1c60b
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-08-20 17:42:36 +0530

    import java.util.Scanner; public class inc {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         int t=sc.nextInt();         int b=sc.nextInt();         System.out.printf("%d KB",s*t*b);     } }

--- Capacity.java
+++ Capacity.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
-public class cap
+public class inc
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
         int s=sc.nextInt();
+        int t=sc.nextInt();
         int b=sc.nextInt();
-        int cap=(2*t*s*b)/2;
-        System.out.printf("%d KB",cap);
+        System.out.printf("%d KB",s*t*b);
     }
 }
